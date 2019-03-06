f070d58b38c781128a542bd965bb59cbca03d985
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-03-06 18:06:07 +0200

    fixed prettier

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Test = () => <h1>App</h1>;
 const About = () => <h1>About</h1>;
