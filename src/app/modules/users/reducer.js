a2abf8dfce67296c0fca496e051158f4ce04fdb9
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-03-26 16:43:46 +0200

    4-reducer

--- src/app/modules/users/reducer.js
+++ src/app/modules/users/reducer.js
@@ -1,4 +1,5 @@
 import { actions, constants } from 'ducks-helpers';
+import { handleActions } from 'redux-actions';
 
 export const TYPE = constants('users', ['~GET_USERS']);
 // users/GET_USERS
@@ -9,3 +10,39 @@ export const ACTION = actions(TYPE);
 // ACTION.getUsers() => { type: 'users/GET_USERS' }
 // ACTION.getUsersSuccess() => { type: 'users/GET_USERS_SUCCESS', payload }
 // ACTION.getUsersError() => { type: 'users/GET_USERS_ERROR', payload }
+
+export const initialState = {
+  isLoading: false,
+  serverError: null,
+
+  data: [],
+};
+
+export default handleActions(
+  {
+    [TYPE.GET_USERS]: setLoading,
+    [TYPE.GET_USERS_SUCCESS]: getUsersSuccess,
+    [TYPE.GET_USERS_ERROR]: setServerError,
+  },
+  initialState,
+);
+
+function setLoading(state) {
+  return { ...state, isLoading: true };
+}
+
+function setServerError(state, action) {
+  return {
+    ...state,
+    isLoading: false,
+    serverError: action.payload,
+  };
+}
+
+function getUsersSuccess(state, action) {
+  return {
+    ...state,
+    isLoading: false,
+    data: action.payload,
+  };
+}
--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import app from 'app/modules/app/reducer';
 import photos from 'app/modules/photos/reducer';
+import users from 'app/modules/users/reducer';
 
 export const rootReducer = combineReducers({
   app,
   photos,
+  users,
 });
